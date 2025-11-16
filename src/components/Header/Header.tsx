8a5bd4b9767d160306aa7668baa225de3c09b165
Viktor Zhurbin <vzhurbin@gmail.com>
2025-11-16 17:35:12 +0100

    hide home button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,10 @@
 import { DarkModeToggle } from '~/ui/DarkModeToggle/DarkModeToggle';
 import styles from './Header.module.css';
-import { HomeButton } from './HomeButton';
 
 export const Header = () => {
   return (
     <div className={styles.root}>
-      <div className={styles.innerWrapper}>
-        <HomeButton />
-      </div>
+      <div className={styles.innerWrapper}></div>
 
       <div className={styles.innerWrapper}>
         <DarkModeToggle />
